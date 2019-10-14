27f6d48050b7d3213cb5f2e3a12ab703f7d8d0ac
jwei98 <justin.lj.wei@gmail.com>
2019-10-14 18:44:58 -0400

    Remove an exclamation mark from main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Circles!!!'
+    return 'Hello, Circles!!'
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
