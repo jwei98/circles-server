328694514db72978a2dcefb85ddfb9009a1c4d9f
Justin Wei <19638700+jwei98@users.noreply.github.com>
2019-10-26 18:18:23 -0400

    Connect Flask Server to Neo4j (#5)
    * update README
    
    * Initia attempt to connect flask server to neo4j db. Uses cloud datastore for env vars
    
    * Clean up neo4j connection
    
    * Final clean

---
+++ auth.py
@@ -0,0 +1,12 @@
+"""
+All authentication convenience functions.
+"""
+from google.cloud import datastore
+
+# Gets the host, username, and password from Datastore.
+def neo4j_creds():
+    # Get auth variables from cloud datastore.
+    datastore_client = datastore.Client()
+    query = datastore_client.query(kind='GaeEnvSettings')
+    env_vars = list(query.fetch())[0]
+    return env_vars['NEO4J_HOST'], env_vars['NEO4J_USERNAME'], env_vars['NEO4J_PASSWORD']
--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+"""
+Main driver for Flask server.
+"""
+import os
+
 from flask import Flask
+from py2neo import Graph
+
+import auth
+from Models import Person
 
 app = Flask(__name__)
+
+# Connect to Neo4j graph.
+host, username, password = auth.neo4j_creds()
+graph = Graph(host=host, username=username, password=password, secure=True)
+
 
 @app.route('/')
 def hello():
     return 'Hello, Circles!!'
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
