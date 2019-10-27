c1e044afdaa038f4b4d18cb420bffab958d04106
jwei98 <justin.lj.wei@gmail.com>
2019-10-27 18:16:08 -0400

    Conform Model.py to data models defined in spec

--- Models.py
+++ Models.py
@@ -6,50 +6,44 @@ from py2neo.ogm import (GraphObject, Property, Related, RelatedTo, RelatedFrom)
 
 
 class Person(GraphObject):
-    __primarykey__ = "username"
-
-    username = Property()
     display_name = Property()
-    uid = Property()
     email = Property()
     photo = Property()
 
     Knows = Related("Person", "KNOWS")
-    Circles = RelatedTo("Circle", "PART_OF")
+    IsMember = RelatedTo("Circle", "IS_MEMBER")
     InvitedTo = Related("Event", "INVITED_TO")
 
-    def __init__(self, username, display_name, email, photo):
-        self.username = username
+    def __init__(self, display_name, email, photo):
         self.display_name = display_name
         self.email = email
         self.photo = photo
 
 
 class Circle(GraphObject):
-
-    name = Property()
+    display_name = Property()
     description = Property()
 
-    Contains = Related("Person", "CONTAINS")
+    HasMember = Related("Person", "HAS_MEMBER")
     Scheduled = Related("Event", "SCHEDULED")
 
-    def __init__(self, name, description):
-        self.name = name
+    def __init__(self, display_name, description):
+        self.display_name = display_name
 
 
 class Event(GraphObject):
-
-    name = Property()
+    display_name = Property()
     description = Property()
+    location = Property()
     datetime = Property()
 
-    InvitedTo = RelatedFrom("Person", "INVITED_TO")
     BelongsTo = Related("Circle", "BELONGS_TO")
 
-    def __init__(self, name, description, time, circle):
-        self.name = name
+    def __init__(self, display_name, description, location, datetime, circle):
+        self.display_name = display_name
         self.description = description
-        self.datetime = time
+        self.location = location 
+        self.datetime = datetime
         # Events should always be linked to a circle.
         self.BelongsTo.add(circle)
 
