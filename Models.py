334b9b1f7298d9217ad4c86ddbffacfa3356de00
jwei98 <justin.lj.wei@gmail.com>
2019-10-27 12:21:01 -0400

    Clean up data models

--- Models.py
+++ Models.py
@@ -8,14 +8,12 @@ from py2neo.ogm import (GraphObject, Property, Related, RelatedTo, RelatedFrom)
 class Person(GraphObject):
     __primarykey__ = "username"
 
-    # Properties.
     username = Property()
     display_name = Property()
     uid = Property()
     email = Property()
     photo = Property()
 
-    # Relationships.
     Knows = Related("Person", "KNOWS")
     Circles = RelatedTo("Circle", "PART_OF")
     InvitedTo = Related("Event", "INVITED_TO")
@@ -26,29 +24,25 @@ class Person(GraphObject):
         self.email = email
         self.photo = photo
 
+
 class Circle(GraphObject):
 
-    # Properties.
-
     name = Property()
+    description = Property()
 
-    # Relationships.
     Contains = Related("Person", "CONTAINS")
     Scheduled = Related("Event", "SCHEDULED")
 
-    def __init__(self, name, members, events):
+    def __init__(self, name, description):
         self.name = name
-        # Initialize ID and Neo relationships
+
 
 class Event(GraphObject):
-
-    # Properties.
 
     name = Property()
     description = Property()
     datetime = Property()
 
-    # Relationships.
     InvitedTo = RelatedFrom("Person", "INVITED_TO")
     BelongsTo = Related("Circle", "BELONGS_TO")
 
@@ -56,5 +50,6 @@ class Event(GraphObject):
         self.name = name
         self.description = description
         self.datetime = time
-        # is there a way to initialize the neo relationships
+        # Events should always be linked to a circle.
+        self.BelongsTo.add(circle)
 
