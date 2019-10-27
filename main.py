8d31deac30171c3590c3cefcea4a75456a9ac60e
Rishiraj Tripathy <rt125@duke.edu>
2019-10-27 17:32:47 -0400

    dummy json for get person request

--- main.py
+++ main.py
@@ -24,74 +24,84 @@ def hello():
 
 @app.route('circles/api/v1.0/users/<int:user_id>', methods=['GET'])
 def get_person(user_id):
-    return
+    return """""{"person": {
+        "username": "cooldude",
+		"display_name": "Cool Dude", 
+		"uid": "stuff",
+		"email": "cooldude@duke.edu:,
+		"photo": "base64",
+		"Knows": [],
+		"Circles": [],
+		"InvitedTo": [],
+    }
+}"""
 
 
-@app.route('circles/api/v1.0/users/<int:user_id>', methods=['POST'])
-def add_person(user_id):
-    if not request.json:
-        abort(400)
-    # Use Circle model to add to graph
-
-    return
-
-
-@app.route('circles/api/v1.0/users/<int:user_id>', methods=['PUT'])
-def update_person(user_id):
-    if not request.json:
-        abort(400)
-    # Use Circle model to add to graph
-
-    return
-
-# Circle API
-
-
-@app.route('circles/api/v1.0/circles/<int:circle_id>', methods=['GET'])
-def get_circle(circle_id):
-    return
-
-
-@app.route('circles/api/v1.0/circles/<int:circle_id>', methods=['POST'])
-def create_circle(circle_id):
-    if not request.json:
-        abort(400)
-    # Use Circle model to add to graph
-
-    return
-
-
-@app.route('circles/api/v1.0/circles/<int:circle_id>', methods=['PUT'])
-def update_circle(circle_id):
-    if not request.json:
-        abort(400)
-    # Use Circle model to update in graph
-
-    return
-# Event API
-
-
-@app.route('circles/api/v1.0/events/<int:event_id>', methods=['GET'])
-def get_circle(event_id):
-    return
-
-
-@app.route('circles/api/v1.0/events/<int:event_id>', methods=['POST'])
-def create_event(event_id):
-    if not request.json:
-        abort(400)
-    # Use Event model to create and link to graph
-
-    return
-
-
-@app.route('circles/api/v1.0/events/<int:event_id>', methods=['PUT'])
-def update_event(event_id):
-    if not request.json:
-        abort(400)
-    # Use Event model to update in graph
-
-    return
+# @app.route('circles/api/v1.0/users/<int:user_id>', methods=['POST'])
+# def add_person(user_id):
+#     if not request.json:
+#         abort(400)
+#     # Use Circle model to add to graph
+#
+#     return
+#
+#
+# @app.route('circles/api/v1.0/users/<int:user_id>', methods=['PUT'])
+# def update_person(user_id):
+#     if not request.json:
+#         abort(400)
+#     # Use Circle model to add to graph
+#
+#     return
+#
+# # Circle API
+#
+#
+# @app.route('circles/api/v1.0/circles/<int:circle_id>', methods=['GET'])
+# def get_circle(circle_id):
+#     return
+#
+#
+# @app.route('circles/api/v1.0/circles/<int:circle_id>', methods=['POST'])
+# def create_circle(circle_id):
+#     if not request.json:
+#         abort(400)
+#     # Use Circle model to add to graph
+#
+#     return
+#
+#
+# @app.route('circles/api/v1.0/circles/<int:circle_id>', methods=['PUT'])
+# def update_circle(circle_id):
+#     if not request.json:
+#         abort(400)
+#     # Use Circle model to update in graph
+#
+#     return
+# # Event API
+#
+#
+# @app.route('circles/api/v1.0/events/<int:event_id>', methods=['GET'])
+# def get_circle(event_id):
+#     return
+#
+#
+# @app.route('circles/api/v1.0/events/<int:event_id>', methods=['POST'])
+# def create_event(event_id):
+#     if not request.json:
+#         abort(400)
+#     # Use Event model to create and link to graph
+#
+#     return
+#
+#
+# @app.route('circles/api/v1.0/events/<int:event_id>', methods=['PUT'])
+# def update_event(event_id):
+#     if not request.json:
+#         abort(400)
+#     # Use Event model to update in graph
+#
+#     return
 
 
 if __name__ == '__main__':
