5e36fbcb18cc2e744a370e6482a3fa92957b617b
rishi-tripathy <40015320+rishi-tripathy@users.noreply.github.com>
2019-10-28 12:58:07 -0400

    [WIP] Populating API stubs with dummy JSON (#7)
    * WIP: routes for api stubs
    
    * stubs without data models created
    
    * removed reliance on ids
    
    * dummy json for get person request
    
    * cleaned dummy json
    
    * match to data model
    
    * get stubs complete with dummy json
    
    * fixed typo in url
    
    * fixed typo in events json
    
    * Fix mock api's by using json library
    
    * remove unnecessary imports
    
    * remove unnecessary comment
    
    * remove uneccesary imports

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 Main driver for Flask server.
 """
 import os
+import json
 
 from flask import Flask
-from py2neo import Graph, NodeMatcher
+from py2neo import Graph
 
 import auth
 from Models import Person, Circle, Event
@@ -19,6 +20,56 @@ graph = Graph(host=host, username=username, password=password, secure=True)
 @app.route('/')
 def hello():
     return 'Hello, Circles!!'
+
+
+@app.route('/circles/api/v1.0/users/<int:person_id>', methods=['GET'])
+def get_person(person_id):
+    person_info = {
+        'Person': {
+            'id': person_id,
+            'display_name': 'Cool Dude', 
+            'email': 'cooldude@duke.edu',
+            'photo': 'base64',
+            'Knows': [],
+            'IsMember': [],
+            'InvitedTo': []
+        }
+    }
+    return json.dumps(person_info)
+
+
+@app.route('/circles/api/v1.0/circles/<int:circle_id>', methods=['GET'])
+def get_circle(circle_id):
+    circle_info = {
+        'Circle': {
+            'id': circle_id,
+            'display_name': 'Circle Display Name',
+            'description': 'Awesome Description',
+            'HasMember': [1, 2, 3],
+            'Scheduled': [4, 5, 6]
+        }
+    }
+    return json.dumps(circle_info)
+
+
+@app.route('/circles/api/v1.0/events/<int:event_id>', methods=['GET'])
+def get_event(event_id):
+    event_info = {
+        'Event': {
+            'id': event_id,
+            'display_name': 'Event Display Name',
+            'description': 'Cool Event Description',
+            'location': 'Location String',
+            'datetime': 'Datetime Object',
+            'BelongsTo': 000,
+            'Invited': {
+                1: True,
+                2: True,
+                3: False,
+            }
+        }
+    }
+    return json.dumps(event_info)
 
 
 
