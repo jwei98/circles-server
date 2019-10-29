759478d901307d91163f4f7e75babe326ca64d06
Rishiraj Tripathy <rt125@duke.edu>
2019-10-29 17:46:09 -0400

    condensed  urls for person-specific requests

--- main.py
+++ main.py
@@ -30,43 +30,36 @@ def hello():
     return 'Hello, Circles!!'
 
 
-@app.route('/circles/api/v1.0/users/', defaults={'person_id': None})
-@app.route('/circles/api/v1.0/users/<int:person_id>', methods=['GET'])
-def get_person(person_id):
+@app.route('/circles/api/v1.0/users/', defaults={'person_id': None,})
+@app.route('/circles/api/v1.0/users/<int:person_id>/', defaults={'resource': None})
+@app.route('/circles/api/v1.0/users/<int:person_id>/<resource>', methods=['GET'])
+def get_person(person_id, resource):
     # Request all users.
     if not person_id:
         return jsonify(Person=get_all_nodes(Person))
-
-    # Request specific user.
+    # Fetch the person
     person = Person.match(graph, person_id).first()
     if not person:
+        print("NO")
         abort(404, description="Resource not found")
-    return jsonify(Person=person.json_repr())
+    if not resource:
+        # Request specific user.
+        return jsonify(Person=person.json_repr())
 
-
-@app.route('/circles/api/v1.0/users/<int:person_id>/circles', methods=['GET'])
-def get_person_circles(person_id):
-    if not person_id:
-        return get_circle(None)
-    person = Person.match(graph, person_id).first()
-    if not person:
-        abort(404, description="resource not found")
+    # Request specific resource associated with the person
     output = []
-    for c in person.IsMember:
-        output.append(c.json_repr())
-    return jsonify(output)
+    if resource not in ['circles', 'events', 'knows']:
+        abort(404, description="Invalid resource specified")
 
-
-@app.route('/circles/api/v1.0/users/<int:person_id>/events', methods=['GET'])
-def get_person_events(person_id):
-    if not person_id:
-        return get_event(None)
-    person = Person.match(graph, person_id).first()
-    if not person:
-        abort(404, description="resource not found")
-    output = []
-    for e in person.InvitedTo:
-        output.append(e.json_repr())
+    if resource == 'circles':
+        for c in person.IsMember:
+            output.append(c.json_repr())
+    if resource == 'events':
+        for e in person.InvitedTo:
+            output.append(e.json_repr())
+    if resource == 'knows':
+        for k in person.Knows:
+            output.append(k.json_repr())
     return jsonify(output)
 
 
