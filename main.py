aaf401e604400943d7800e9c7942021028e79ee6
Rishiraj Tripathy <rt125@duke.edu>
2019-10-29 17:20:50 -0400

    added ability to query for circles by user

--- main.py
+++ main.py
@@ -44,6 +44,32 @@ def get_person(person_id):
     return jsonify(Person=person.json_repr())
 
 
+@app.route('/circles/api/v1.0/users/<int:person_id>/circles', methods=['GET'])
+def get_person_circles(person_id):
+    if not person_id:
+        return get_circle(None)
+    person = Person.match(graph, person_id).first()
+    if not person:
+        abort(404, description="resource not found")
+    output = []
+    for c in person.IsMember:
+        output.append(c.json_repr())
+    return jsonify(output)
+
+
+@app.route('/circles/api/v1.0/users/<int:person_id>/events', methods=['GET'])
+def get_person_circles(person_id):
+    if not person_id:
+        return get_event(None)
+    person = Person.match(graph, person_id).first()
+    if not person:
+        abort(404, description="resource not found")
+    output = []
+    for e in person.InvitedTo:
+        output.append(e.json_repr())
+    return jsonify(output)
+
+
 @app.route('/circles/api/v1.0/circles/', defaults={'circle_id': None})
 @app.route('/circles/api/v1.0/circles/<int:circle_id>', methods=['GET'])
 def get_circle(circle_id):
