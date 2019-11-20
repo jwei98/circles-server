a54ac683ef7a04784265a0daebd7fbde6dc7a910
jwei98 <justin.lj.wei@gmail.com>
2019-11-20 12:34:25 -0500

    Remove test.json

--- test_json.py
+++
@@ -1,5 +0,0 @@
-import json,urllib.request
-url = 'http://127.0.0.1:8080/circles/api/v1.0/users/202'
-data = urllib.request.urlopen(url).read()
-output = json.loads(data.decode('utf-8'))
-print(output)
