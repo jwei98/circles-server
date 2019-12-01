7f417efe3877caf8d60a10c9c62b9f615caff407
Rishiraj Tripathy <rt125@duke.edu>
2019-12-01 02:20:27 -0500

    Gating resource access to GET requests

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def person(person_id, resource=None):
 
     #Fetch the person making the request
     req_token = request.headers.get('Authorization')
-    req_user = Person.match(graph).where('_.email = {}'.format(req_token))
+    req_user = Person.match(graph).where('_.email = {}'.format(req_token)).first()
 
     #Fetch the person requested
     person = Person.match(graph, person_id).first()
